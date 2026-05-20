259ac21b4f097286b3ee0f1f52092806d73e00a4
23235aa <23235aa@hvhs.school.nz>
2026-05-21 10:59:26 +1200

    forms

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ console.log("Running Sal's Strawberries")
 function writeForm(){
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
+    let fruit = favoriteFruit.value;
+    const Name_field = document.getElementById("name").value;
+    let username2 = Name_field.value;
+    const fruitquantity = document.getElementById("fruitQuantity").value;
+    let quantiity = fruitquantity.value;
 }
 var username = prompt("what is your name ")
 var favFood = prompt("Hello  "+username+". What is your favorite fruit?")
-console.log(username + " likes " + favFood)
+console.log(username2 + " likes " + fruit)
 
-firebase.database().ref('/Food/users/' + username).set(
+firebase.database().ref('/Foods/users/' + username).set(
     favFood
   );
+
+  function fb_write(){
+
+  }
