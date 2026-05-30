258a667222c7e2920aa09f26b0fcda4b2a49fb93
23235aa <23235aa@hvhs.school.nz>
2026-05-30 13:45:21 +1200

    clean up

--- script.js
+++ script.js
@@ -21,16 +21,8 @@ firebase.database().ref('/Foods/users/' + GLOBAL_user.uid).update(
    
  HTML_OUTPUT.innerHTML = GLOBAL_user.displayName + " likes " + favoriteFruit + " They have it " + fruitquantity+ " times"
 }
- function showEmail(){
-    console.log("Reading email");
-  firebase.database().ref('/Food').once('value', fb_displayEmail, fb_readError)
- }
  function fb_readError(error) {
     console.log("There was an error reading reading the message");
     console.error(error);
   };
-  //unction fb_displayEmail(snapshot){
-    //console.log(snapshot.val())
-//let Email = snapshot.val()
-//console.log("Your favortie fruit is " + Email)
-  //}
+  
